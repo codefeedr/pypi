95f4ae81de2b8911b5faf036e49c661bfe30753a
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-04-30 15:35:31 +0200

    Fixes!

--- src/main/scala/org/codefeedr/pypi/Main.scala
+++ src/main/scala/org/codefeedr/pypi/Main.scala
@@ -1,7 +1,18 @@
 package org.codefeedr.pypi
 
+import org.codefeedr.pipeline.PipelineBuilder
+
 object Main {
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    val releaseSource = new PyPiReleasesStage()
+    val enrichReleases = new PyPiReleasesExtStage()
+
+    new PipelineBuilder()
+      .setBufferProperty("message.max.bytes", "5000000") // max message size is 5mb
+      .setBufferProperty("max.request.size", "5000000") // max message size is 5 mb
+      .edge(releaseSource, enrichReleases)
+      .start(args)
+  }
 
 }
