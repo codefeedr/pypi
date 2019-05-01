75ca4f08c37ef726afbf51b624c7031a4e1aa720
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-05-01 12:46:10 +0200

    Added import.

--- src/main/scala/org/codefeedr/pypi/Main.scala
+++ src/main/scala/org/codefeedr/pypi/Main.scala
@@ -1,17 +1,22 @@
 package org.codefeedr.pypi
 
 import org.codefeedr.pipeline.PipelineBuilder
+import org.codefeedr.plugins.pypi.stages.{
+  PyPiReleaseExtStage,
+  PyPiReleasesStage
+}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     val releaseSource = new PyPiReleasesStage()
-    val enrichReleases = new PyPiReleasesExtStage()
+    val enrichReleases = new PyPiReleaseExtStage()
 
     new PipelineBuilder()
       .setBufferProperty("message.max.bytes", "5000000") // max message size is 5mb
       .setBufferProperty("max.request.size", "5000000") // max message size is 5 mb
       .edge(releaseSource, enrichReleases)
+      .build()
       .start(args)
   }
 
