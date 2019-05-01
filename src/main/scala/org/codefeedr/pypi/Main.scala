ffa7286f7a975573f7e9488b28c6fdc6d4664019
Wouter Zorgdrager <zorgdragerw@gmail.com>
2019-05-01 12:48:09 +0200

    Added backend and restart strategy.

--- src/main/scala/org/codefeedr/pypi/Main.scala
+++ src/main/scala/org/codefeedr/pypi/Main.scala
@@ -1,5 +1,10 @@
 package org.codefeedr.pypi
 
+import java.util.concurrent.TimeUnit
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.common.time.Time
+import org.codefeedr.buffer.KafkaBuffer
 import org.codefeedr.pipeline.PipelineBuilder
 import org.codefeedr.plugins.pypi.stages.{
   PyPiReleaseExtStage,
@@ -13,6 +18,14 @@ object Main {
     val enrichReleases = new PyPiReleaseExtStage()
 
     new PipelineBuilder()
+      .setPipelineName("PyPi plugin")
+      .setRestartStrategy(RestartStrategies.fixedDelayRestart(
+        3,
+        Time.of(10, TimeUnit.SECONDS))) // try restarting 3 times
+      .enableCheckpointing(1000) // checkpointing every 1000ms
+      .setBufferProperty(KafkaBuffer.COMPRESSION_TYPE, "gzip")
+      .setBufferProperty(KafkaBuffer.BROKER, "localhost:29092")
+      .setBufferProperty(KafkaBuffer.ZOOKEEPER, "localhost:2181")
       .setBufferProperty("message.max.bytes", "5000000") // max message size is 5mb
       .setBufferProperty("max.request.size", "5000000") // max message size is 5 mb
       .edge(releaseSource, enrichReleases)
