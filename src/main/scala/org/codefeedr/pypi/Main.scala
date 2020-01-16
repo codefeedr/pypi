236c6de32d66d3fe9c48bdd0faf6ee5211e1505f
Wouter Zorgdrager <zorgdragerw@gmail.com>
2020-01-16 08:56:05 +0100

    Update PyPi.

--- src/main/scala/org/codefeedr/pypi/Main.scala
+++ src/main/scala/org/codefeedr/pypi/Main.scala
@@ -22,8 +22,10 @@ object Main {
       .setRestartStrategy(RestartStrategies.fixedDelayRestart(
         3,
         Time.of(10, TimeUnit.SECONDS))) // try restarting 3 times
-      .enableCheckpointing(1000) // checkpointing every 1000ms
+      .enableCheckpointing(5000) // checkpointing every 5000ms
       .setBufferProperty(KafkaBuffer.COMPRESSION_TYPE, "gzip")
+      .setBufferProperty(KafkaBuffer.AMOUNT_OF_PARTITIONS, "8")
+      .setBufferProperty(KafkaBuffer.AMOUNT_OF_REPLICAS, "2")
       .setBufferProperty(KafkaBuffer.BROKER, "kafka-0.kafka-headless.codefeedr:9092,kafka-1.kafka-headless.codefeedr:9092,kafka-2.kafka-headless.codefeedr:9092")
       .setBufferProperty(KafkaBuffer.ZOOKEEPER, "zookeeper-0.zookeeper-headless.codefeedr:2181,zookeeper-1.zookeeper-headless.codefeedr:2181,zookeeper-2.zookeeper-headless.codefeedr:2181")
       .setBufferProperty("message.max.bytes", "10485760") // max message size is 10mb
